cb192256266fd493be0436d17e857aa48e1c0f3b
Sarfaraz Nawaz <sarfaraz.nawaz@tenx.tech>
2020-07-03 14:38:59 +0530

    part10: move image fns from Main.hs to Image.hs

--- src/Image.hs
+++ src/Image.hs
@@ -2,10 +2,16 @@
 module Image where
 
 import           Data.Maybe
+import           Data.List     (intercalate)
+import           System.IO     (IOMode (WriteMode), hFlush, hPutStrLn, withFile)
+import           System.Random
+import           Text.Printf (printf)
 
 import           BaseVec
+import           Camera
 import           Hittable
 import           Ray
+import           Utils
 import           Vec
 import           Colors
 
@@ -14,6 +20,36 @@ data Image = Image {
         height :: Int,
         pixels :: [[Color]]
     } deriving (Show)
+
+createImage :: (Hittable a) => Int -> Int -> Int -> [Double] -> a -> Image
+createImage width height samplePerPixels randList world = Image {
+    width = width,
+    height = height,
+    pixels = do
+        j <- reverse [0..height - 1]
+        return $ do
+            i <- [0..width - 1]
+            let rands = take samplePerPixels randList
+            let sampledColor = foldr (+) (SampledColor(samplePerPixels, vec 0 0 0)) $ fmap (randomRayColor i j world) rands
+            return $ toColor sampledColor
+    }
+        where
+            randomRayColor i j world rand = toSampledColor samplePerPixels $ rayColor (ray camera u v) world
+                where
+                    u = (fromIntegral i + rand) / fromIntegral (width - 1)
+                    v = (fromIntegral j + rand) / fromIntegral (height - 1)
+
+writeImage :: (Hittable a) => Int -> Int -> a -> IO ()
+writeImage imageWidth samplePerPixels world = do
+    let gen = mkStdGen 22
+    let randList = (randomRs (0,1) gen) :: [Double]
+    let image = createImage imageWidth (floor (fromIntegral imageWidth / aspectRatio)) samplePerPixels randList world
+    let fmtColor (BaseVec [r,g,b]) = printf "%3d %3d %3d\n" r g b
+
+    putStrLn "P3"
+    putStrLn $ show (width image) ++ " " ++ show (height image)
+    print 255
+    putStrLn $ unlines [ intercalate "    " [fmtColor c | c <- row] | row <- pixels image]
 
 rayColor :: (Hittable a) => Ray -> a -> Color
 rayColor ray@(Ray origin direction) world = toColor $ fromMaybe default_color $ do
--- src/Main.hs
+++ src/Main.hs
@@ -8,19 +8,9 @@
 
 module Main where
 
-import           Data.List     (intercalate)
-import           System.IO     (IOMode (WriteMode), hFlush, hPutStrLn, withFile)
-import           Text.Printf (printf)
-import           System.Random
-
 import           BaseVec
-import           Camera
 import           Hittable
 import           Image
-import           Ray
-import           Utils
-import           Vec
-import           Colors
 
 samplePerPixels = 100
 
@@ -33,29 +23,13 @@ main :: IO ()
 main = do
     -- writeImage 1800
     -- writeImage 800
-    writeImage 384
+    writeImage 384 samplePerPixels world
     -- testTools
-
-writeImage imageWidth = do
-    let gen = mkStdGen 22
-    let randList = (randomRs (0,1) gen) :: [Double]
-    let image = createImage imageWidth (floor (fromIntegral imageWidth / aspectRatio)) randList
-    let fmtColor (BaseVec [r,g,b]) = printf "%3d %3d %3d\n" r g b
-
-    putStrLn "P3"
-    putStrLn $ show (width image) ++ " " ++ show (height image)
-    print 255
-    putStrLn $ unlines [ intercalate "    " [fmtColor c | c <- row] | row <- pixels image]
-
 
 kolor :: Kolor -> Kolor
 kolor a = a
 
 testTools = do
-    let gen = mkStdGen 22
-    let r = (randomRs (0,1) gen) :: [Double]
-    print $ take 5 r
-    print $ take 6 r
     let n = 2 :: Int
     let a = (from 5)  :: Kolor
     let b = (vec 1 2 3) :: Kolor
@@ -81,21 +55,3 @@ testTools = do
     -- print $ a .* n
     -- print $ a ./ n
     print "DONE"
-
-createImage :: Int -> Int -> [Double] -> Image
-createImage width height randList = Image {
-    width = width,
-    height = height,
-    pixels = do
-        j <- reverse [0..height - 1]
-        return $ do
-            i <- [0..width - 1]
-            let rands = take samplePerPixels randList
-            let sampledColor = foldr (+) (SampledColor(samplePerPixels, vec 0 0 0)) $ fmap (randomRayColor i j world) rands
-            return $ toColor sampledColor
-    }
-        where
-            randomRayColor i j world rand = toSampledColor samplePerPixels $ rayColor (ray camera u v) world
-                where
-                    u = (fromIntegral i + rand) / fromIntegral (width - 1)
-                    v = (fromIntegral j + rand) / fromIntegral (height - 1)
