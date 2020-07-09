195bc69a6f9dac4635ba7038ece93e3eaa48aa2e
Sarfaraz Nawaz <sarfaraz.nawaz@tenx.tech>
2020-07-10 04:28:14 +0530

    SECTION 8.5: True Lambertian Reflection

--- src/Image.hs
+++ src/Image.hs
@@ -12,7 +12,7 @@ import           Camera         (camera, rayAt)
 import           Colors         (Color, ColorVec, SampledColor (SampledColor), toColor)
 import           Hittable       (HitRecord (HitRecord), Hittable (hit), HittableList (HittableList), Sphere (Sphere))
 import           Ray            (Ray (Ray))
-import           Samplings      (sampleFraction, samplePointInSphere)
+import           Samplings      (sampleFraction, sampleUnitVector)
 import           Vec            (unit, vec, yCoor, zero, (.*))
 
 data Image = Image {
@@ -60,13 +60,13 @@ writeImage = do
 rayColor :: (Hittable a, RandomGen g) => Ray -> a -> g -> Int -> (ColorVec, g)
 rayColor ray@(Ray _origin direction) world g depth =if depth <= 0 then (zero, g) else computeColor
     where
-        h = hit world ray 0 maxValue
+        h = hit world ray 0.001 maxValue
         t = 0.5 * (yCoor (unit direction) + 1.0)
         default_color = (vec 1 1 1) .* (1.0 -t) + (vec 0.5 0.7 1.0) .* t
         computeColor = fromMaybe (default_color, g) $ do
                                                 (HitRecord p normal _ _ ) <- h
-                                                let (pointInUnitSphere, g1) = samplePointInSphere g 1
-                                                let target = p + normal + pointInUnitSphere
+                                                let (sampled, g1) = sampleUnitVector g
+                                                let target = p + normal + sampled
                                                 let newRay = Ray p (target - p)
                                                 let (newColor, g2) = rayColor newRay world g1 (depth - 1)
                                                 return $ (newColor .* 0.5, g2)
