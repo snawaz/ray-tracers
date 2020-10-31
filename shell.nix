fceee112cbc4541b936965f823e2dfe4f31456c2
Providence Salumu <Providence.Salumu@smunix.com>
2020-10-31 08:36:57 -0400

    nix: add shell.nix

---
+++ shell.nix
@@ -0,0 +1,45 @@
+{ nixpkgsPath ? <nixpkgs>, compiler ? "default", doBenchmark ? false }:
+
+let
+
+  inherit (import nixpkgsPath {
+    overlays = [
+      (self: super: rec {
+        fllvm = super.llvm.overrideAttrs (_: rec {
+          version = "7.1.0";
+        });
+      })
+    ];
+  }) pkgs;
+  f = { mkDerivation, array, base, deepseq, directory
+      , numeric-limits, parallel, pretty-terminal, random, stdenv, time
+      , fllvm
+      }:
+      mkDerivation {
+        pname = "ray-tracers";
+        version = "0.2.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          array base deepseq directory numeric-limits parallel
+          pretty-terminal random time
+        ];
+        librarySystemDepends = [
+          fllvm
+        ];
+        license = "unknown";
+        hydraPlatforms = stdenv.lib.platforms.none;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  variant = if doBenchmark then pkgs.haskell.lib.doBenchmark else pkgs.lib.id;
+
+  drv = variant (haskellPackages.callPackage f {});
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
